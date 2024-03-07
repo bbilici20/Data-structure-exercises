fdca32e964e26e436d090c3f03b9611a4a71cf45
bbilici20 <98103357+bbilici20@users.noreply.github.com>
2024-03-07 10:13:03 +0100

    Update cutoff_ranks.py
    comments added

--- cutoff_ranks.py
+++ cutoff_ranks.py
@@ -2,11 +2,11 @@ def countLevelUpPlayers(cutOffRank, num, scores):
     meximum=max(scores)
     ranks=[0]*(meximum+1)
     for score in scores:
-        ranks[meximum-score]+=1
+        ranks[meximum-score]+=1 #use counting sort to sort all employee scores
     count=0
     counter=1
     for ranik in ranks:
-        if counter<=cutOffRank and ranik!=0:
+        if counter<=cutOffRank and ranik!=0: #if there are people with that score add the number of people with that score
             count+=ranik
             counter+=ranik
     return count
